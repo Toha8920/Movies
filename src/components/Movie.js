44a37689c466d367bc7a18fc3f0b1966d0e0aa69
Toha Islam <islamtoha710@gmail.com>
2022-12-28 20:42:01 +0600

    projects done

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,12 +3,25 @@ const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
 const Movie = ({ movie }) => {
     const { title, poster_path, overview, vote_average } = movie;
+
+    const setVoteClass = (vote) => {
+        if (vote >= 8) {
+            return 'green'
+        }
+        else if (vote => 6) {
+            return 'orange'
+        }
+        else {
+            return 'red'
+        }
+    }
+
     return (
         <div className='movie'>
-            <img src={IMG_API + poster_path} alt="" />
+            <img src={(poster_path ? (IMG_API + poster_path) : 'https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2059&q=80')} alt="" />
             <div className="movie-info">
                 <h3>{title}</h3>
-                <span>{vote_average}</span>
+                <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
             </div>
             <div className="movie-over">
                 <h2>Overview:</h2>
